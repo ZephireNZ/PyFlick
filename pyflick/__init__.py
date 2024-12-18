2098a3df75817ae284b89f003b79ebea96bb70a0
ZephireNZ <brynley+github@zephire.nz>
2024-12-18 20:08:23 +1300

    Use library for JSON::API parsing

--- pyflick/__init__.py
+++ pyflick/__init__.py
@@ -1,100 +1,47 @@
 """Python API For Flick Electric in New Zealand"""
 from .authentication import AbstractFlickAuth
 from .const import DEFAULT_API_HOST
-from .types import JsonApiResponse, CustomerAccount, CustomerIcpConsumer, RatingRatedPeriod, RatingComponent
-from typing import List
+from .types import CustomerAccount, RatingRatedPeriod, APIException, UnauthorizedException, FlickPrice
 
-from dateutil.parser import isoparse
-from datetime import datetime as dt
-from decimal import Decimal
-
-
-class FlickPrice():
-    def __init__(self, pricing: RatingRatedPeriod):
-        attributes = pricing["attributes"]
-
-        self.price: Decimal = Decimal(attributes["cost"])
-        self.start_at: dt = isoparse(attributes["start_at"])
-        self.end_at: dt = isoparse(attributes["end_at"])
-        self.status: str = attributes["type"]
-        self.components: List[PriceComponent] = [
-            PriceComponent(c) for c in getComponents(pricing)]
-        self.raw = pricing
-
-    def __repr__(self):
-        return f"FlickPrice({self.raw})"
-
-
-class PriceComponent():
-    def __init__(self, component: RatingComponent):
-        # TODO: Fix attributes
-        # attributes = component["attributes"]
-
-        self.kind: str = component["kind"]
-        self.charge_method: str = component["charge_method"]
-        self.charge_setter: str = component["charge_setter"]
-        self.value: Decimal = Decimal(component["value"])
-        self.quantity: Decimal = Decimal(component["quantity"])
-        self.unit_code: str = component["unit_code"]
-        self.per: str = component["per"]
-        self.flow_direction: str = component["flow_direction"]
-        self.metadata: dict = component["metadata"]
-        self.raw = component
-
-    def __repr__(self):
-        return f"PriceComponent({self.raw})"
+import json_api_doc
 
 
 class FlickAPI():
     """Python API For Flick Electric in New Zealand"""
+
     def __init__(self, auth: AbstractFlickAuth, host: str = DEFAULT_API_HOST):
         self._auth: AbstractFlickAuth = auth
         self._host: str = host
 
-    async def getCustomerAccounts(self) -> list[CustomerAccount]:
-        """Returns the accounts viewable by the current user."""
-
-        response = await self._auth.request("GET", "/customer/v1/accounts", params={
-            "include": "main_consumer"
-        })
+    async def __getJsonDoc(self, *args, **kwargs):
+        response = await self._auth.request(*args, **kwargs)
 
         async with response:
+            if (response.status in [401, 403]):
+                raise UnauthorizedException()
             if response.status != 200:
                 raise APIException({
                     "status": response.status,
                     "message": await response.text()
                 })
 
-            api_response: JsonApiResponse[list[CustomerAccount]] = await response.json()
+            api_response = await response.json()
 
-            return api_response["data"]
+            return json_api_doc.deserialize(api_response)
+
+    async def getCustomerAccounts(self) -> list[CustomerAccount]:
+        """Returns the accounts viewable by the current user."""
+
+        return await self.__getJsonDoc("GET", "/customer/v1/accounts", params={
+            "include": "main_consumer"
+        })
 
     async def getPricing(self, supply_node: str) -> FlickPrice:
         """Gets current pricing for the given supply node."""
-        response = await self._auth.request("GET", "rating/v1/rated_periods", params={
+
+        period: RatingRatedPeriod = await self.__getJsonDoc("GET", "rating/v1/rated_periods", params={
             "include": "components",
             "supply_node_ref": supply_node,
         })
 
-        async with response:
-            if response.status != 200:
-                raise APIException({
-                    "status": response.status,
-                    "message": await response.text()
-                })
-
-            api_response: JsonApiResponse[RatingRatedPeriod] = await response.json()
-
-            return FlickPrice(api_response["data"])
-
-
-class APIException(Exception):
-    pass
-
-
-def getSupplyNode(account: CustomerAccount) -> CustomerIcpConsumer:
-    return next([x for x in account["included"] if x["type"] == CustomerIcpConsumer.type])
-
-
-def getComponents(period: RatingRatedPeriod) -> list[RatingComponent]:
-    return [x for x in period["included"] if x["type"] == RatingComponent.type]
+        return FlickPrice(period)
--- pyflick/types.py
+++ pyflick/types.py
@@ -1,36 +1,32 @@
-from typing import TypedDict, Self
+from typing import TypedDict, Literal, Optional
+from dateutil.parser import isoparse
+from datetime import datetime as dt
+from decimal import Decimal
 
 
-class JsonApiResponse[TData](TypedDict):
-    data: TData
-
-
-class JsonApiResource[TAttrib](TypedDict):
-    id: str
-    type: str
-    attributes: TAttrib
-    included: list[Self]
-
-
-class CustomerAccountAttributes(TypedDict):
-    account_number: str
-
-
-class CustomerAccount(JsonApiResource[CustomerAccountAttributes]):
-    type = "customer_account"
-
-
-class CustomerIcpConsumerAttributes(TypedDict):
+class CustomerIcpConsumer(TypedDict):
+    type = "customer_icp_consumer"
     icp_number: str
     supply_node_ref: str
     physical_address: str
 
 
-class CustomerIcpConsumer(JsonApiResource[CustomerIcpConsumerAttributes]):
-    type = "customer_icp_consumer"
+class CustomerAccount(TypedDict):
+    type = "customer_account"
+    account_number: str
+    address: str
+    status: Literal["pending", "cancelled", "active", "closed"]
+    main_consumer: Optional[CustomerIcpConsumer]
 
 
-class RatingRatedPeriodAttributes(TypedDict):
+class RatingComponent(TypedDict):
+    # TODO: Attributes
+    type = "rating_component"
+    pass
+
+
+class RatingRatedPeriod(TypedDict):
+    type = "rating_rated_period"
     start_at: str
     end_at: str
     status: str
@@ -43,12 +39,44 @@ class RatingRatedPeriodAttributes(TypedDict):
     export_quantity: str
     quantity_unit: str
     renewable_quantity: str
+    components: list[RatingComponent]
 
 
-class RatingRatedPeriod(JsonApiResource[RatingRatedPeriodAttributes]):
-    type = "rating_rated_period"
+class APIException(Exception):
+    pass
 
 
-# TODO: Attributes
-class RatingComponent(JsonApiResource):
-    type = "rating_component"
+class UnauthorizedException(Exception):
+    pass
+
+
+class FlickPrice():
+    def __init__(self, period: RatingRatedPeriod):
+        self.raw: RatingRatedPeriod = period
+        self.price: Decimal = Decimal(period["cost"])
+        self.start_at: dt = isoparse(period["start_at"])
+        self.end_at: dt = isoparse(period["end_at"])
+        self.status: str = period["type"]
+        self.components: list[PriceComponent] = [PriceComponent(c) for c in period["components"]]
+
+    def __repr__(self):
+        return f"FlickPrice({self.raw})"
+
+
+class PriceComponent():
+    def __init__(self, component: RatingComponent):
+        # TODO: Fix attributes
+
+        self.raw = component
+        self.kind: str = component["kind"]
+        self.charge_method: str = component["charge_method"]
+        self.charge_setter: str = component["charge_setter"]
+        self.value: Decimal = Decimal(component["value"])
+        self.quantity: Decimal = Decimal(component["quantity"])
+        self.unit_code: str = component["unit_code"]
+        self.per: str = component["per"]
+        self.flow_direction: str = component["flow_direction"]
+        self.metadata: dict = component["metadata"]
+
+    def __repr__(self):
+        return f"PriceComponent({self.raw})"
